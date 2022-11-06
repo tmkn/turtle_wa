3937bf3615d8aeff2b42a7638c84e108a4847593
Thomas Klein <tmkn@users.noreply.github.com>
2022-11-06 22:23:47 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+fn main() -> std::io::Result<()> {
+    let ttl_path = Path::new("./ttl/simple.ttl");
+    let file_result = File::open(ttl_path);
+
+    let mut file = match file_result {
+        Ok(file) => file,
+        Err(error) => {
+            println!("Couldn't find file {}", ttl_path.display());
+            return Err(error);
+        }
+    };
+
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents)?;
+
+    println!("{}", contents);
+    println!("{}", count_diamond_brackets(&contents));
+
+    Ok(())
+}
+
+fn count_diamond_brackets(input: &str) -> usize {
+    let mut count = 0;
+    for c in input.chars() {
+        if c == '<' {
+            count += 1;
+        }
+    }
+    count
 }
