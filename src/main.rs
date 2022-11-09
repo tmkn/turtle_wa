2b7bfa48347e7b1d4e148be6dc474147078ef6ae
Thomas Klein <tmkn@users.noreply.github.com>
2022-11-09 18:35:34 +0000

    wip parser

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,12 @@ fn main() -> std::io::Result<()> {
 
     file.read_to_string(&mut contents)?;
 
-    parse_line(&contents);
+    let mut num_lines: u32 = 1;
+    for line in contents.lines() {
+        parse_line(line, num_lines);
+
+        num_lines += 1;
+    }
 
     Ok(())
 }
@@ -26,20 +31,17 @@ fn main() -> std::io::Result<()> {
 fn parse_iri(input: &str) -> Option<String> {
     let mut iri = String::new();
     let mut end_token = false;
-    let start_token = input.chars().nth(0);
+    let start_token = input.chars().next();
 
-    match start_token {
-        Some('<') => {
-            for c in input[1..].chars() {
-                if c == '>' {
-                    end_token = true;
-                    break;
-                } else {
-                    iri.push(c);
-                }
+    if let Some('<') = start_token {
+        for c in input[1..].chars() {
+            if c == '>' {
+                end_token = true;
+                break;
+            } else {
+                iri.push(c);
             }
         }
-        _ => {}
     }
 
     match end_token {
@@ -48,48 +50,49 @@ fn parse_iri(input: &str) -> Option<String> {
     }
 }
 
-fn parse_line(input: &str) -> () {
+fn parse_line(input: &str, line_num: u32) -> () {
     let mut current_offset: usize = 0;
-    let mut remaining_str = &input[current_offset..];
 
     while current_offset < input.len() {
-        current_offset += count_whitespace(remaining_str);
-
-        remaining_str = &input[current_offset..];
+        let remaining_str = &input[current_offset..];
 
         let start_token = remaining_str.chars().nth(0);
 
         match start_token {
+            Some(' ') | Some('\t') => {
+                // skip whitespace
+                current_offset += 1;
+            }
             Some('<') => {
                 let iri = parse_iri(remaining_str);
 
-                match iri {
-                    Some(ref parsed_iri) => {
-                        current_offset += parsed_iri.chars().count() + 2;
-                    }
-                    _ => {}
+                if let Some(ref parsed_iri) = iri {
+                    current_offset += parsed_iri.chars().count() + 2;
                 }
 
                 println!("IRI: {}", iri.unwrap_or(String::from("NOT FOUND")));
             }
+            Some('"') => {
+                println!("Todo ({line_num}:{current_offset}): Parse literal: {remaining_str}");
+                current_offset += remaining_str.len();
+            }
+            Some('#') => {
+                println!(
+                    "Todo ({line_num}:{current_offset}): Parse comment: {}",
+                    remaining_str
+                );
+                current_offset += remaining_str.len();
+            }
             Some('.') => {
                 current_offset += 1;
+            }
+            Some(token) => {
+                println!(
+                    "Error ({line_num}:{current_offset}): Unexpected token '{token}' in {remaining_str}"
+                );
+                current_offset += remaining_str.len();
             }
             _ => {}
         }
     }
-}
-
-fn count_whitespace(input: &str) -> usize {
-    let mut offset = 0;
-
-    for c in input.chars() {
-        match c {
-            ' ' => offset += 1,
-            '\t' => offset += 1,
-            _ => break,
-        }
-    }
-
-    offset
 }
