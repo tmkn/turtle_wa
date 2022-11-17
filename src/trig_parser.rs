4688d525e8846a99144eb7c138d46d045e71f512
Thomas Klein <tmkn@users.noreply.github.com>
2022-11-17 22:28:16 +0100

    wip lexer

--- src/trig_parser.rs
+++ src/trig_parser.rs
@@ -158,6 +158,8 @@ pub fn next_word(itr: &mut Peekable<Enumerate<Chars>>) -> Option<String> {
 
 #[cfg(test)]
 mod tests {
+    use crate::trig_parser::lexer::{read_iri_lexeme, Token};
+
     use super::*;
 
     #[test]
@@ -181,5 +183,188 @@ mod tests {
         let mut itr = "               ".chars().enumerate().peekable();
 
         assert_eq!(next_word(&mut itr), None);
+    }
+
+    #[test]
+    fn test_lexer_absolute_or_relative_iri_lexeme() {
+        let mut itr = "<foo>".chars().enumerate().peekable();
+
+        assert_eq!(
+            read_iri_lexeme(&mut itr),
+            Token::RelativeOrAbsoluteIri(String::from("foo"))
+        );
+
+        let mut itr = "<http://example.org/#batman>"
+            .chars()
+            .enumerate()
+            .peekable();
+
+        assert_eq!(
+            read_iri_lexeme(&mut itr),
+            Token::RelativeOrAbsoluteIri(String::from("http://example.org/#batman"))
+        );
+
+        let mut itr = "     <foo>       ".chars().enumerate().peekable();
+
+        assert_eq!(
+            read_iri_lexeme(&mut itr),
+            Token::RelativeOrAbsoluteIri(String::from("foo"))
+        );
+
+        let mut itr = "<foo".chars().enumerate().peekable();
+
+        assert_eq!(read_iri_lexeme(&mut itr), Token::Unknown(None));
+
+        let mut itr = "<foo bar>".chars().enumerate().peekable();
+
+        assert_eq!(read_iri_lexeme(&mut itr), Token::Unknown(None));
+    }
+
+    #[test]
+    fn test_lexer_prefix_iri_lexeme() {
+        let mut itr = "foo:bar ".chars().enumerate().peekable();
+
+        assert_eq!(
+            read_iri_lexeme(&mut itr),
+            Token::PrefixIri(String::from("foo:bar"))
+        );
+
+        let mut itr = ":foobar ".chars().enumerate().peekable();
+
+        assert_eq!(
+            read_iri_lexeme(&mut itr),
+            Token::PrefixIri(String::from(":foobar"))
+        );
+
+        let mut itr = "foobar: ".chars().enumerate().peekable();
+
+        assert_eq!(
+            read_iri_lexeme(&mut itr),
+            Token::PrefixIri(String::from("foobar:"))
+        );
+
+        let mut itr = "foo:bar,".chars().enumerate().peekable();
+
+        assert_eq!(
+            read_iri_lexeme(&mut itr),
+            Token::PrefixIri(String::from("foo:bar"))
+        );
+
+        let mut itr = "foobar".chars().enumerate().peekable();
+
+        assert_eq!(read_iri_lexeme(&mut itr), Token::Unknown(None));
+    }
+}
+
+mod lexer {
+    use std::{
+        iter::{Enumerate, Peekable},
+        str::Chars,
+        usize,
+    };
+
+    #[derive(PartialEq, Debug)]
+    pub enum Token {
+        RelativeOrAbsoluteIri(String),
+        PrefixIri(String),
+        Unknown(Option<String>),
+    }
+
+    pub fn read_iri_lexeme(itr: &mut Peekable<Enumerate<Chars>>) -> Token {
+        let mut lexeme = String::new();
+
+        skip_whitespace(itr);
+
+        match (itr.peek()) {
+            Some((_, '<')) => {
+                // absolute IRI
+                let lexeme = read_absolute_or_relative_iri_lexeme(itr);
+
+                match lexeme {
+                    Some(Token::RelativeOrAbsoluteIri(lexeme)) => {
+                        return Token::RelativeOrAbsoluteIri(lexeme);
+                    }
+                    _ => return Token::Unknown(None),
+                }
+            }
+            Some((_, _)) => {
+                // relative IRI
+                let lexeme = read_prefix_iri_lexeme(itr);
+
+                match lexeme {
+                    Some(Token::PrefixIri(lexeme)) => {
+                        return Token::PrefixIri(lexeme);
+                    }
+                    _ => return Token::Unknown(None),
+                }
+            }
+            _ => {
+                // unknown
+                return Token::Unknown(None);
+            }
+        }
+    }
+
+    fn read_absolute_or_relative_iri_lexeme(itr: &mut Peekable<Enumerate<Chars>>) -> Option<Token> {
+        let mut lexeme = String::new();
+        let mut found_end_token = false;
+        let mut found_whitespace = false;
+
+        let start_token = itr.next();
+
+        match start_token {
+            Some((_, c)) if c != '<' => return None,
+            _ => {}
+        }
+
+        while let Some((_, c)) = itr.next() {
+            if c == '>' {
+                found_end_token = true;
+                break;
+            } else if c.is_whitespace() {
+                found_whitespace = true;
+                break;
+            }
+
+            lexeme.push(c);
+        }
+
+        match (found_end_token, found_whitespace) {
+            (true, false) => Some(Token::RelativeOrAbsoluteIri(lexeme)),
+            _ => None,
+        }
+    }
+
+    fn read_prefix_iri_lexeme(itr: &mut Peekable<Enumerate<Chars>>) -> Option<Token> {
+        let mut lexeme = String::new();
+        let mut colon_token = false;
+        let mut found_end_token = false; // ' ' | ',' coma is used for object list
+
+        while let Some((_, c)) = itr.peek() {
+            if c == &':' {
+                colon_token = true;
+            } else if c.is_whitespace() || c == &',' {
+                found_end_token = true;
+                break;
+            }
+
+            lexeme.push(*c);
+            itr.next();
+        }
+
+        match (colon_token, found_end_token) {
+            (true, true) => Some(Token::PrefixIri(lexeme)),
+            _ => None,
+        }
+    }
+
+    fn skip_whitespace(itr: &mut Peekable<Enumerate<Chars>>) {
+        while let Some((_, c)) = itr.peek() {
+            if c.is_whitespace() {
+                itr.next();
+            } else {
+                break;
+            }
+        }
     }
 }
